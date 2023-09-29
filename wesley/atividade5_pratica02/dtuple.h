1288f90eb119685f86eaafc43384df0e0f34ffac
richardgss <142852268+richardgss@users.noreply.github.com>
2023-09-28 23:10:06 -0300

    Create dtuple.h

---
+++ wesley/atividade5_pratica02/dtuple.h
@@ -0,0 +1,15 @@
+//
+// Created by Richard on 28/09/2023.
+//
+
+#ifndef D_TUPLE_H
+#define D_TUPLE_H
+
+typedef struct d_tuple_int {
+  int length;
+  int* data;
+} tuple_int;
+
+
+
+#endif //D_TUPLE_H
