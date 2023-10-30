1702e89401aa5ce847085f864ea11c544b54abc7
richardgss <142852268+richardgss@users.noreply.github.com>
2023-10-30 19:24:26 -0300

    Create pratica_03_grafos_direcionados.c

---
+++ wesley/pratica_03_grafos_direcionados.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void informa_distancias(int grafo[3][3]) {
+    
+}
+
+int main()
+{
+    printf("Hello World");
+
+    // Debug only
+    grafo[0][0] = 0;
+    grafo[0][1] = 4;
+    grafo[0][2] = 0;
+    grafo[1][0] = 4;
+    grafo[1][1] = 0;
+    grafo[1][2] = 2;
+    grafo[2][0] = 0;
+    grafo[2][1] = 2;
+    grafo[2][2] = 0;
+    grafo[3][0] = 5;
+    grafo[3][1] = 4;
+    grafo[3][2] = 3;
+
+    return 0;
+}
